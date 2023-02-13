5aa334e4a5f0ee3a565608ad3b4621c42d193234
Anastasiia578 <119096333+Anastasiia578@users.noreply.github.com>
2023-02-13 21:50:38 +0200

    j

--- modal.js/menu.js
+++ modal.js/menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector("[data-Menu-open]"),
     closeMenuBtn: document.querySelector("[data-Menu-close]"),
     menu: document.querySelector("[data-Menu]"),
+    body: document.querySelector("[body]"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
